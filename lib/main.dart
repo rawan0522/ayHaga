1295922791868eb1178fd9564abc44d8af544b59
rawan.reda <rawan0522075@gmail.com>
2024-11-07 09:21:12 +0200

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo',//////////////
       debugShowCheckedModeBanner: false,
       home: ChangeNotifierProvider(
           create:(context) => ToDoProvider(),
